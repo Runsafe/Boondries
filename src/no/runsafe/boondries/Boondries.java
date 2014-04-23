8a8d35e86b067456c334fd4fdc87a73954bf219a
Kruithne <kruithne@gmail.com>
2014-04-23 18:22:11 +0100

    We don't need the command sub-system anymore.

--- src/no/runsafe/boondries/Boondries.java
+++ src/no/runsafe/boondries/Boondries.java
@@ -1,7 +1,6 @@
 package no.runsafe.boondries;
 
 import no.runsafe.framework.RunsafeConfigurablePlugin;
-import no.runsafe.framework.features.Commands;
 import no.runsafe.framework.features.Events;
 
 public class Boondries extends RunsafeConfigurablePlugin
@@ -10,7 +9,6 @@ public class Boondries extends RunsafeConfigurablePlugin
 	protected void pluginSetup()
 	{
 		// Framework features
-		addComponent(Commands.class);
 		addComponent(Events.class);
 
 		// Plugin components
